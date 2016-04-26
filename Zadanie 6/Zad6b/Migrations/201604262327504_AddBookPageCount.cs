504f74960b76683b06f51284e2455803276a2663
rbierowiec <bierowiecr@gmail.com>
2016-04-27 01:29:28 +0200

    Dodanie migracji bazy do zadania 6.

---
+++ Zadanie 6/Zad6b/Migrations/201604262327504_AddBookPageCount.cs
@@ -0,0 +1,39 @@
+namespace Zad6b.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBookPageCount : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "public.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AuthorName = c.String(),
+                        AuthorSurname = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "public.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        BookTitle = c.String(),
+                        ISBN = c.String(),
+                        pageCount = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("public.Books");
+            DropTable("public.Authors");
+        }
+    }
+}
--- Zadanie 6/Zad6b/Models/Book.cs
+++ Zadanie 6/Zad6b/Models/Book.cs
@@ -10,5 +10,6 @@ namespace Zad6b.Models
         public int Id { get; set; }
         public string BookTitle { get; set; }
         public string ISBN { get; set; }
+        public int pageCount { get; set; }
     }
 }
